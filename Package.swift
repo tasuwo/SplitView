3ee395add6596c3d523c8ab452ee4b2c92202034
tasuwo <kamuhata.you@gmail.com>
2024-12-25 00:35:09 +0900

    :heavy_minus_sign: 不要な依存を削除

--- Package.swift
+++ Package.swift
@@ -1,21 +1,17 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SplitView",
     platforms: [
-        .iOS("16"), .macOS(.v12)
+        .iOS("16"), .macOS(.v12),
     ],
     products: [
         .library(
             name: "SplitView",
             targets: ["SplitView"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-format", .upToNextMajor(from: "509.0.0"))
+        )
     ],
     targets: [
         .target(name: "SplitView")
